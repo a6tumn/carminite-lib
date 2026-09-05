24d7edaaa4be1252cb1a0b63b6d40c4fdcd27c5d
Autumn <shinigami7x@proton.me>
2026-09-05 14:00:24 -0400

    Added new item extension.

--- src/main/java/carminite/interfaces/extensions/IItemExtension.java
+++ src/main/java/carminite/interfaces/extensions/IItemExtension.java
@@ -1,9 +1,13 @@
 package carminite.interfaces.extensions;
 
+import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.jetbrains.annotations.ApiStatus;
 
 public interface IItemExtension {
 	default int carminite$getMaxStackSize(ItemStack stack) {
@@ -12,5 +16,10 @@ public interface IItemExtension {
 
 	default boolean carminite$onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
 		return false;
+	}
+
+	@ApiStatus.OverrideOnly
+	default boolean carminite$supportsEnchantment(ItemStack stack, Holder<Enchantment> enchantment) {
+		return stack.is(Items.ENCHANTED_BOOK) || enchantment.value().isSupportedItem(stack);
 	}
 }
